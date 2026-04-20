1867d686c83cd5c1bcd136a5d2b628dc92eed219
ssep <ssep4u@gmail.com>
2026-04-20 14:48:54 +0900

    TodoListApp deleteTodo()

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,7 +1,7 @@
 import Checkbox from './Checkbox.jsx'
 import Button from './Button.jsx'
 
-export default function TodoItem({ todo, toggleTodo }) {
+export default function TodoItem({ todo, toggleTodo, deleteTodo }) {
     return (
         // todo.isCompleted가 true 면 " todo__item--complete", false 면 ""
         <li className={`todo__item${todo.isCompleted ? " todo__item--complete" : ""}`}>
@@ -11,7 +11,10 @@ export default function TodoItem({ todo, toggleTodo }) {
                 onChange={() => toggleTodo(todo.id)}
             >{todo.text}</Checkbox>
             <Button className='todo__button todo__button--edit'>✏️</Button>
-            <Button className='todo__button todo__button--delete'>❌</Button>
+            <Button
+                className='todo__button todo__button--delete'
+                onClick={() => deleteTodo(todo.id)}
+            >❌</Button>
         </li>
     )
 }
